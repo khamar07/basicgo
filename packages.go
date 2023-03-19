79efde3a594a8aa42e334baa543abafbc6443467
khamar07 <94104939+khamar07@users.noreply.github.com>
2023-03-19 13:50:22 +0530

    Create packages.go

---
+++ packages.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	randomNumber := rand.Intn(10)
+	fmt.Printf("My favorite number is %d\n", randomNumber)
+}
